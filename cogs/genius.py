a15b0145c95298518b136ed01105fa348f80b039
Voctor <voctorbidoof@gmail.com>
2018-08-22 19:53:07 -0700

    Lyric searching now a function

---
+++ cogs/genius.py
@@ -0,0 +1,37 @@
+import discord
+from discord.ext import commands
+
+import lyricsgenius as genius
+
+from datetime import datetime
+
+import requests
+import json
+
+# load config file
+with open('config.json') as data_file:
+    key = json.load(data_file)["genius"]
+
+api = genius.Genius(key)
+
+class Genius:
+    def __init__(self, client):
+        self.client = client
+
+    @commands.command(pass_context=True)
+    async def search(self, ctx):
+        words = ctx.message.clean_content.split(" ")
+
+        # songId = api.search_genius(" ".join(words[1:]))["hits"][0]["result"]["id"]
+        lyrics = api.search_song(" ".join(words[1:]))
+        # print(lyrics.lyrics)
+
+        embed = discord.Embed(title=f'Lyrics for "{lyrics.title}" by {lyrics.artist}',
+                description=lyrics.lyrics,
+                color=0x801ecc)
+
+        await self.client.say("", embed = embed)
+
+
+def setup(client):
+    client.add_cog(Genius(client))
---
+++ main.py
@@ -0,0 +1,72 @@
+import discord
+from discord.ext import commands
+
+import asyncio
+import json
+
+# load config file
+with open('config.json') as data_file:
+    data = json.load(data_file)
+
+# set prefix
+client = commands.Bot(command_prefix = data["prefix"])
+
+# load cogs
+extensions = ['cogs.genius']
+
+# checks for owner perms
+def ownerPerms(ctx):
+    return ctx.message.author.id in data["owners"]
+
+# triggered when bot is logged in
+@client.event
+async def on_ready():
+    print('Bot online.')
+
+# load cogs command
+@client.command()
+@commands.check(ownerPerms)
+async def load(extension):
+    try:
+        client.load_extension('cogs.{}'.format(extension))
+        print('Loaded {}'.format(extension))
+        await client.say('Loaded `{}`.'.format(extension))
+    except Exception as error:
+        print('{} cannot be loaded. [{}]'.format(extension, error))
+        await client.say('`{}` cannot be loaded. [{}]'.format(extension, error))
+
+# unload cogs command
+@client.command()
+@commands.check(ownerPerms)
+async def unload(extension):
+    try:
+        client.unload_extension('cogs.{}'.format(extension))
+        print('Unloaded {}'.format(extension))
+        await client.say('Unloaded {}.'.format(extension))
+    except Exception as error:
+        print('`{}` cannot be unloaded. [{}]'.format(extension, error))
+        await client.say('`{}` cannot be unloaded. [{}]'.format(extension, error))
+
+# reload cogs command
+@client.command()
+@commands.check(ownerPerms)
+async def reload(extension):
+    try:
+        client.unload_extension('cogs.{}'.format(extension))
+        client.load_extension('cogs.{}'.format(extension))
+        print('Reloaded {}'.format(extension))
+        await client.say('Reloaded `{}`.'.format(extension))
+    except Exception as error:
+        print('{} cannot be reloaded. [{}]'.format(extension, error))
+        await client.say('`{}` cannot be reloaded. [{}]'.format(extension, error))
+
+# something to do with cogs
+if __name__ == '__main__':
+    for extension in extensions:
+        try:
+            client.load_extension(extension)
+        except Exception as error:
+            print('{} cannot be loaded. [{}]'.format(extension, error))
+
+# login
+client.run(data["discord"])
