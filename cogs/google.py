a7e4f48a4856fa8120a9da2d9b4e95a16085628d
Voctor <voctorbidoof@gmail.com>
2018-09-03 13:54:20 -0700

    im sleep

--- cogs/google.py
+++ cogs/google.py
@@ -62,7 +62,7 @@ class Google:
                 embed.add_field(name=res["name"], value=res["info"], inline=False)
 
                 # convert date to datetime
-                date = datetime.strptime(res["info"].split("(")[0].strip().replace(",", ""), '%B %d %Y').total_seconds()
+                date = datetime.strptime(res["info"].split("(")[0].strip().replace(",", ""), '%B %d %Y').time()
 
                 if comparison["youngest"]["name"] == False:
                     comparison["youngest"]["name"] = res["name"]
