938e1b750a9cb92f2a125e6e2ef11368968648dc
Voctor <voctorbidoof@gmail.com>
2018-09-02 23:15:10 -0700

    corrected the big dumb

--- cogs/google.py
+++ cogs/google.py
@@ -174,8 +174,8 @@ def find_coordinates (msg):
     longitude = page.cssselect(".longitude")[0].text_content()
     information += "\n" + longitude
 
-    latitude.replace("°", ".").replace("′", "").replace("″", "")
-    longitude.replace("°", ".").replace("′", "").replace("″", "")
+    latitude = latitude.replace("°", ".").replace("′", "").replace("″", "")
+    longitude = longitude.replace("°", ".").replace("′", "").replace("″", "")
 
     if "N" in latitude:
         latitude = float(latitude.replace("N", ""))
