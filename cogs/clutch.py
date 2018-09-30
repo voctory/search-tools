4fc6b2c71f7f04123a3a73e906d26417508b307c
Voctor <voctorbidoof@gmail.com>
2018-09-30 10:09:01 -0700

    testing

--- cogs/clutch.py
+++ cogs/clutch.py
@@ -17,8 +17,22 @@ class Clutch:
             await self.client.say("You need to mention someone!")
             return
 
+        if ctx.message.mentions[0].id in clutch_list:
+            await self.client.say("They are already being voted on! Scroll above.")
+            return
+
+        if ctx.message.author.id == ctx.message.mentions[0].id:
+            await self.client.say("You can't clutch yourself.")
+            return
+
+        if ctx.message.author.bot == True:
+            await self.client.say("You are a bot.")
+            return
+
+        clutch_list.append(ctx.message.mentions[0].id)
+
         embed = discord.Embed(title="Clutch",
-                description=f'React if you believe {ctx.message.mentions[0].mention} has clutched!\nVote within 15 seconds.',
+                description=f'React if you believe {ctx.message.mentions[0].mention} has clutched!\nVote within 15 seconds. At least 4 people must agree and it has to be a majority.',
                 color=0x801ecc)
         msg = await self.client.say("", embed=embed)
         await self.client.add_reaction(msg, "👍")
@@ -27,9 +41,11 @@ class Clutch:
 
         msg = await self.client.get_message(msg.channel, msg.id)
         if msg.reactions[0].count >= 2 and msg.reactions[0].count > msg.reactions[1].count:
-            await self.client.say("Vote passed!")
+            await self.client.say(f'Vote has been passed for {ctx.message.mentions[0].mention}!')
         else:
-            await self.client.say("Vote did not pass.")
+            await self.client.say(f'Vote for {ctx.message.mentions[0].mention} did not pass.')
+
+        clutch_list.remove(ctx.message.mentions[0].id)
 
 
 def setup(client):
