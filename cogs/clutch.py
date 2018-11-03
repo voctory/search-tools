79a69b292e8bfd996a3d13591e3ccd5355b33112
Voctor <voctorbidoof@gmail.com>
2018-11-02 22:07:57 -0700

    test

--- cogs/clutch.py
+++ cogs/clutch.py
@@ -48,7 +48,8 @@ class Clutch:
         msg = await self.client.get_message(msg.channel, msg.id)
 
         # TODO: reset valeus
-        print(msg.author == await self.client.get_reaction_users(msg.reactions[0])[0])
+        test = await self.client.get_reaction_users(msg.reactions[0])[0]
+        print(msg.author == test)
 
         if msg.reactions[0].count > 4 and msg.reactions[0].count > msg.reactions[1].count:
             await self.client.say(f'Vote has been passed for {ctx.message.mentions[0].mention}!')
